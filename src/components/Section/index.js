43b04b0867077d7ac57398e773f85bbef7454ed9
Nick Ray <nickrray@gmail.com>
2020-08-10 12:13:00 -0500

    :construction: Create renderList fxn and utilize in return

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,7 +1,30 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-export const Section = ({ section: { heading, image, text } }) => {
+export const Section = ({ section: { heading, images, list, text, title } }) => {
+
+  const currentSection = title
+
+  console.log('Inside Section:')
+  console.log(currentSection)
+
+  // TODO: Need a switch or check to render list and whole section appropriately
+
+  const renderList = () => {
+    return list.map((li, i) => {
+      return (
+        // TODO: remove hard coded li class and apply based on currentSection
+        <li className='feature-list-item flex' key={i}>
+          <img
+            className={li.image.className}
+            src={li.image.src}
+            alt={li.image.alt}
+          />
+          {li.text}
+        </li>
+      )
+    })
+  }
 
   const renderTexts = () => {
     return text.map((txt, i) => {
@@ -15,10 +38,11 @@ export const Section = ({ section: { heading, image, text } }) => {
   }
 
   return (
-    <section className="section">
+    <section className={`section ${title ? 'section--' + title : null}`}>
       {heading ? <h3 className="section-heading">{heading}</h3> : null}
+      {list ? <ul className="list section-text">{renderList()}</ul> : null}
       {text ? renderTexts() : null}
-      {image.src ? <img alt={image.alt} src={image.src} /> : null}
+      {images ? <img alt={images.alt} src={images.src} /> : null}
     </section>
   )
 }
